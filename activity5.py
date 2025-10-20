db5864760d54ac1cc2fb93c87d45c38a191ff5f6
DeanRegos <rego0903@bears.unco.edu>
2025-10-20 17:04:48 +0000

    functions 1 - 3

---
+++ activity5.py
@@ -0,0 +1,34 @@
+# Function 1
+def twenty_or_more(file):
+    words_file = open('CROSSWD.txt', 'r')
+    data = []
+    for x in words_file:
+        if len(x) >= 20:
+            data.append(x)
+    print(data)
+
+
+twenty_or_more('CROSSWD.txt')
+
+# Function 2
+def has_no_e(word):
+    # if 'e' in word:
+    #     return False
+    # else:
+    #     return True
+    
+    check = True
+    for letter in word:
+        if 'e' == letter:
+            check = False
+    return check
+
+# Function 3
+def uses_only(word, letters):
+    for x in word:
+        if x not in letters:
+            return False
+    return True
+
+
+# print(uses_only('abra', 'abr'))
